07bf3bc4a575eda3b8edb7c0aa27cce8e4d7e591
Michael Huso <husomichael@gmail.com>
2022-02-18 16:55:05 -0600

    changed user table

--- database.sql
+++ database.sql
@@ -3,9 +3,8 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "users" (
+CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL,
-    "access" INT NOT NULL,
+    "password" VARCHAR (1000) NOT NULL
 );
