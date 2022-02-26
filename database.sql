c844c53dacee0d9b6ff32e9559d98bb13a1cfb48
Michael Huso <husomichael@gmail.com>
2022-02-26 15:17:51 -0600

    updated database.sql

--- database.sql
+++ database.sql
@@ -3,8 +3,37 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+--Database for qq_bearz
+
+-- "users" table for logging in.
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "soundclips" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user" (id) ON DELETE CASCADE,
+	"title" VARCHAR (80),
+	"tags" VARCHAR (1000),
+	"url" VARCHAR (1000)
+);
+
+CREATE TABLE "arakan_deaths" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user" (id) ON DELETE CASCADE,
+	"title" VARCHAR (80),
+	"tags" VARCHAR (1000),
+	"url" VARCHAR (1000)
+);
+
+CREATE TABLE "arakan_death_count" (
+	"id" SERIAL PRIMARY KEY,
+	"death_count" INT
+);
+
+INSERT INTO "arakan_death_count"
+("death_count")
+VALUES
+(1);
