77ac16a78fda02ed08a095b866fa2ff5e3169e5c
Michael Huso <husomichael@gmail.com>
2022-03-25 11:49:54 -0500

    user selected from redux store to UserProfile

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import {Box, Typography, Grid, Button} from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
 
 function UserProfile() {
 
   const history = useHistory();
+  const user = useSelector((store) => store.user);
+
+  console.log(user);
+
+  //Link profile with Blizzard API
+  //Append account / character info to set profile page.
 
   return (
     <div>
-      User Profile Page
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography variant="h2" sx={{mt: 4}}>
+          {user.username}'s Profile
+        </Typography>
+      </Box>
     </div>
   );
 };
