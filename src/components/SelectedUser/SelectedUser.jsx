71278019bd8bec5977a594edd2a81be9085616d1
Michael Huso <husomichael@gmail.com>
2022-04-21 22:17:31 -0500

    SelectedUser initialized

---
+++ src/components/SelectedUser/SelectedUser.jsx
@@ -0,0 +1,15 @@
+import {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {useHistory} from 'react-router-dom';
+
+function SelectedUser(){
+
+  
+  return(
+    <div>
+
+    </div>
+  )
+};
+
+export default SelectedUser;
